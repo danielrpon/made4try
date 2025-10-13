b01cba560ad4d1192e438631ac2acc6774acf868
Daniel Restrepo Núñez <danielrpon@gmail.com>
2025-10-13 18:07:05 -0500

    Modularización: app en módulos (io_tcx, metrics, plots, export_xlsx, config, utils)

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 # streamlit_app.py (fino)
-from app import *  # o simplemente: ejecuta `streamlit run -m made4try.app`
+from made4try.app import * # o simplemente: ejecuta `streamlit run -m made4try.app`
 
 
 # """
