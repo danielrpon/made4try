9be4318fdfa4976a936b105afdb1d0cf82ee1ae2
Daniel Restrepo Núñez <danielrpon@gmail.com>
2025-11-09 10:20:56 -0500

    fix(deploy): agrega streamlit_app.py como entrypoint para Streamlit Cloud

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,12 @@
-# streamlit_app.py (en la raíz del repo)
+# streamlit_app.py
+import sys, os
+from pathlib import Path
+
+# Asegura que el paquete interno sea visible
+BASE_DIR = Path(__file__).resolve().parent / "made4try"
+sys.path.append(str(BASE_DIR))
+
+# Importa la app principal
 from made4try.app import run
 
 if __name__ == "__main__":
